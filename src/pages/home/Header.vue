a75b92caa3b6534037329b4ff4e5ee5338427813
wenqiS <770503759@qq.com>
2017-11-01 14:10:42 +0800

    anything

---
+++ src/pages/home/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  
+ <header class="header">
+ 	<a class="header-left"><</a>
+ 		<div class="header-title">
+				<span class="single-line">输入城市/景点/游玩主题</span>
+ 		</div>
+ 		<div class="header-right">
+ 				<span class="mp-nav-city">北京</span>	
+ 		</div>
+	</header>
+</template>	
+
+<script>
+
+export default {
+ 
+ 
+}
+</script>
+
+<style scoped>
+.header {
+    position: relative;
+    width: 100%;
+    height: .88rem;
+    background: #00bcd4;
+    text-align: center;
+    color: #fff;
+	}
+	.header-left{
+    width: .4rem;
+    line-height: .88rem;
+    padding: 0 .2rem;
+    color: #fff;
+    font-size: .36rem;
+    text-align: left;
+    float:left;
+}
+	.header-title{
+	position:absolute;
+	top: .14rem;
+	left:.8rem;
+	right:1.28rem;
+	background:#fff;
+	color:#ccc;
+	border-radius: .06rem;
+    line-height: .6rem;
+
+	}	
+	.header-right{
+		line-height:.88rem;
+		width:1.28rem;
+		float:right;
+	}
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <index-header />
+   
+  </div>
+</template>
+
+<script>
+
+import header from "./header.vue";
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    "index-header":header
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
